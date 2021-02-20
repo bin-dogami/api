2ceb6e5dee252c81bc7f53fe5596f8ff1ffaeb84
yangbin185 <yangbin185@jd.com>
2021-02-20 17:59:29 +0800

    update

--- src/interceptor/transform.interceptor.ts
+++ src/interceptor/transform.interceptor.ts
@@ -12,13 +12,23 @@ interface Response<T> {
 }
 @Injectable()
 export class TransformInterceptor<T>
-  implements NestInterceptor<T, Response<T>> {
+  implements NestInterceptor<T, T | Response<T>> {
   intercept(
     context: ExecutionContext,
     next: CallHandler<T>,
-  ): Observable<Response<T>> {
+  ): Observable<T | Response<T>> {
+    // https://docs.nestjs.cn/7/exceptionfilters?id=arguments-host
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest();
+    console.log(request.url)
+
     return next.handle().pipe(
       map(data => {
+        // 网站建设中～ 做特殊处理
+        if (request.url === '/') {
+          return data
+        };
+
         return {
           data,
           code: 0,
