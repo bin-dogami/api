b0946c80876cd8ee556015befcca96a56bd09d15
yangbin185 <yangbin185@jd.com>
2021-03-06 01:28:37 +0800

    update

--- spider/utils/index.js
+++ spider/utils/index.js
@@ -17,78 +17,6 @@ const powMap = {
   '千': 1000,
   '万': 10000,
 }
-// 提取标题中的数字
-// const getIndexFromTitle = (title) => {
-//   let t = `${title}`
-//   let isSureMenuNum = 0
-//   if (t.includes('章')) {
-//     t = t.replace(/章.*/, '').trim()
-//     isSureMenuNum++
-//   }
-//   if (t.includes('第')) {
-//     t = t.replace(/.*第/, '').trim()
-//     isSureMenuNum++
-//   }
-//   if (isSureMenuNum >= 2) {
-//     // 对这样的进行处理： 第901、902、903章 了却因果
-//     const match = t.match(/(?<=[^\d]?)\d+(?=[^\d]?)/g)
-//     if (match && match.length > 1) {
-//       return match
-//     }
-//   }
-//   // 004失去记忆的自己是个食人魔？ 这样没有章也没有第的
-//   if (t === title) {
-//     const matches = t.trim().match(/^(\d+)/)
-//     t = matches && matches.length ? matches[1] : t
-//   }
-
-//   // @TODO: 这里还是有些问题，得判断出是否以数字开头，且数字有效
-//   // 先简单修复 万分抱歉 这样的标题
-//   if (t === title) {
-//     return 0
-//   }
-
-//   t = t.replace(/[^零一二三四五六七八九十百千万\d]/g, '')
-//   // 有汉字数字
-//   if (!/^\d+$/.test(t)) {
-//     // 汉字数字转化为阿拉伯数字，字母加空格好分组
-//     t = t.replace(/./g, (w) => {
-//       if (w in numMap) {
-//         return numMap[w]
-//       }
-//       if (['十', '百', '千', '万'].includes(w)) {
-//         return ` ${w} `
-//       }
-//       return w
-//     }).trim().replace(/^0+/, '')
-//     // 非纯数字
-//     if (/[十百千万]/.test(t)) {
-//       // 把数字和字母分开成一个数组
-//       const arr = t.split(/\s/)
-//       if (arr.length) {
-//         const _arr = []
-//         // 以 十百等 加前面的数字为一组，生成一个数组，最后再累加起来，千/万前面如果找不到数字自动加1
-//         arr.reduce((num, item, index) => {
-//           const _item = item.trim()
-//           if (/[十百千万]/.test(_item)) {
-//             _arr.push((num || 1) * powMap[_item])
-//             return 0
-//           } else if (item === '') {
-//             return num
-//           } else {
-//             if (index === arr.length - 1) {
-//               _arr.push(+_item)
-//             }
-//             return +_item
-//           }
-//         }, 0)
-//         t = _arr.reduce((num, item) => num + item, 0)
-//       }
-//     }
-//   }
-//   t = `${t}`.replace(/^0+/, '')
-//   return +t
-// }
 
 // 从抓取的目录中获取有效数据
 function getMenus ($domMenus, $) {
@@ -219,6 +147,8 @@ const menusAnalysis = ($domMenus, $, lastMenuInfo = null) => {
     }
   }
 
+  // console.log(_menus)
+
   if (lastMenuInfo && !findLastSpiderMenu) {
     return `异常,找不到上一次抓取的最后一章: ${lastOrigTitle}`
   }
@@ -253,7 +183,7 @@ function analysisTitle (title) {
   // index 分析的很细致！
   if (title.includes('章')) {
     // ['第901、902、903章 了却因果', '第一三七章', '第1百七3章']
-    const indexMatch = title.match(/第(.*)章/);
+    const indexMatch = title.match(/第([^第]*)章/);
     // 第30章 标题
     [index, mname, hasNormalIndex, matchFaild] = dealNormNumByMatch(indexMatch, index, mname)
     // 第30章 标题 没匹配上再来匹配一下前面有空格的 30章 标题
