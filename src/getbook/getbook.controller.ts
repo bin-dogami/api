1c6db8df8fbd156ba2424c9e09182b8540d6ba44
yangbin185 <yangbin185@jd.com>
2021-05-28 00:50:33 +0800

    update

--- src/getbook/getbook.controller.ts
+++ src/getbook/getbook.controller.ts
@@ -493,7 +493,7 @@ export class GetBookController {
   }
 
   // @NOTE: 定时任务，每个小时候里每隔10分钟抓取目标网站的新书
-  @Cron('30 12,27,42,57 * * * *')
+  @Cron('30 9,24,39,54 * * * *')
   async cronSpiderBooks() {
     if (process.env.NODE_ENV === 'development') {
       return
